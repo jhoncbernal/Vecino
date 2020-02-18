03b98110a2c029aaa122b2ff735e25942a62c8b8
jhoncbernal <jhoncasallas@Jhons-MacBook-Pro.local>
2020-02-18 18:17:01 -0500

    [FIX]: user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,11 +2,27 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const { compareSync, hashSync, genSaltSync } = require('bcryptjs');
 
-const UserSchema = new Schema({
-    name: { type: String, required: true },
-    username: { type: String, required: true },
-    password: { type: String, required: true }
+const UserSchema = new Schema({    
+    username:           { type: String,  required: true },
+    password:           { type: String,  required: true },
+    email:              { type: String,  required: true },
+    enabled:            { type: Boolean, required: true },
+    roles:              [{type: String, required: true }],
+    firstName:          { type: String,  required: true },
+    lastName:           { type: String,  required: true },
+    phone:              { type: String,  required: true },
+    neighborhoodcode:   { type: String,  required: true },
+    neighborhood:{
+        type:Schema.Types.ObjectId,
+        ref:"neighborhood",
+        required:true,
+        autopopulate:{ select: 'neighborhoodname' }
+    },
 });
+UserSchema.path('email').validate(function (email) {
+    var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailRegex.test(email); // Assuming email has a text attribute
+ }, 'The e-mail field cannot be empty or with out email structure.')
 UserSchema.methods.toJSON = function () {
     let user = this.toObject();
     delete user.password;
@@ -27,5 +43,5 @@ UserSchema.pre('save', async function (next) {
     user.password = hashedPassword;
     next();
 })
-
+UserSchema.plugin(require("mongoose-autopopulate"))
 module.exports = mongoose.model('user', UserSchema);

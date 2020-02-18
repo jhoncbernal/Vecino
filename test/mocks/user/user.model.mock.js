f9aeb8d76ae9771d1d576a58a26c10b09e889255
jhoncbernal <jhoncasallas@Jhons-MacBook-Pro.local>
2020-02-18 10:08:19 -0500

    [ADD]: test for user

---
+++ test/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+    user: {
+        "_id": "5e3d9bea456ee0208c48a941",
+        "name": "Jhon",
+        "username": "Jhoncbernal",
+        "password": "MyContraseña"
+    },
+    users: [
+        {
+            "_id": "5e3da1acd06ed227542c9a9f",
+            "name": "Juan",
+            "username": "Juanito",
+            "password": "MyContraseña"
+        },
+        {
+            "_id": "5e3da1bcd06ed227542c9aa0",
+            "name": "Pedro",
+            "username": "Pedrito",
+            "password": "MyContraseña"
+        }
+    ]
+};

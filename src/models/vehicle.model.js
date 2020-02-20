17da7dc4c80b98465e0d07bbe7c007e5561fe810
jhoncbernal <jhoncasallas@Jhons-MacBook-Pro.local>
2020-02-19 19:57:14 -0500

    [FIX]: fixed vehicle model neighboorhoodname

--- src/models/vehicle.model.js
+++ src/models/vehicle.model.js
@@ -3,10 +3,11 @@ const { Schema } = mongoose;
 
 const VehicleSchema = new Schema({    
     vehicletype:        { type: String,  required: true },
+    username:           { type: String},
     plate:              { type: String,  required: true },
     position:           { type: String },
     enabled:            { type: Boolean, required: true },
-    parkingplan:        { type: String },
+    parkingplan:        { type: String, required: true },
     user:{
         type:Schema.Types.ObjectId,
         ref:"user",
@@ -17,7 +18,7 @@ const VehicleSchema = new Schema({
             'email',
             'HomeNumber',
             'BlockNumber',
-            'neighborhood.neighborhoodname', ]}
+            'neighborhood neighborhoodname' ]}
     }
 });
 

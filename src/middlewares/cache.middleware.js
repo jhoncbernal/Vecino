c5b2aa7b8786543d5b760cbedd351dda2da1d384
jhoncbernal <jhoncasallas@Jhons-MacBook-Pro.local>
2020-02-20 17:11:11 -0500

    [FIX]:fixed key const values

--- src/middlewares/cache.middleware.js
+++ src/middlewares/cache.middleware.js
@@ -3,7 +3,7 @@ const {CACHE_KEY}=require('../config');
 
 module.exports=function(duration){
     return (req,res,next)=>{
-        const key = CACHE_KEY+req.originUrl||req.url;
+        const key = CACHE_KEY+req.originalUrl;
         const cacheBody= mcache.get(key);
         if(cacheBody){
             return res.send(JSON.parse(cacheBody));

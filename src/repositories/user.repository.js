a31fa807fccc3aa123c01fe90374ed53c3e23c44
jhoncbernal <jhoncasallas@Jhons-MacBook-Pro.local>
2020-02-26 17:53:48 -0500

    [FIX]:send email with estructure html

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,5 +1,8 @@
 const BaseRepository = require('./base.repository');
 const nodemailer = require('nodemailer');
+var handlebars = require('handlebars');
+var fs = require('fs');
+var path = require('path'); 
 const { FROM_EMAIL, PSWD_EMAIL } = require('../config');
 let _user = null;
 class UserRepository extends BaseRepository {
@@ -35,9 +38,7 @@ class UserRepository extends BaseRepository {
                 })
                 .then(user => {
                     return sendEmail(user, "Password change request",
-                        `Hi ${user.username} \n 
-                        Please click on the following link "http://"${host}"/api/auth/reset/${user.resetPasswordToken} to reset your password. \n\n 
-                        If you did not request this, please ignore this email and your password will remain unchanged.\n`,)
+                        `http://${host}/v1/api/auth/reset/${user.resetPasswordToken}`,(path.join(__dirname, '../public/pages/recoverypassword.html')))
                         .then((result) => {
                             return { ...{"message":"Password change request"},... { "email":{result} } };
                         }).catch((error) => {
@@ -87,10 +88,12 @@ class UserRepository extends BaseRepository {
                 return user.save()
             })
             .then(user => {
+              let  date =Math.floor(Date.now() / 1000);
                 return sendEmail(user,
-                    "Your password has been changed",
-                    `Hi ${user.username} \n 
-                    This is a confirmation that the password for your account ${user.email} has just been changed.\n`)
+                    "Your password has been changed",`:
+
+                    El dia de hoy, se realizo exitosamente el cambio de contraseña a su cuenta , ${user.email} `,
+                    (path.join(__dirname, '../public/pages/passwordreset.html')))
                     .then((result) => {
                         return { ...{"message":"Your password has been changed"},... { "email":{result} } };
                     }).catch((error) => {
@@ -103,34 +106,64 @@ class UserRepository extends BaseRepository {
     }
 
 }
-async function sendEmail(user, subject, text) {
 
+async function sendEmail(user, subject, text,htmlpath) {
+    var readHTMLFile = function(path, callback) {
+        fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+            if (err) {
+                throw err;
+                callback(err);
+            }
+            else {
+                callback(null, html);
+            }
+        });
+    };
+    
+  let mailOptions=  readHTMLFile(htmlpath, function(err, html) {
+        var template = handlebars.compile(html);
+        var replacements = {
+             username: user.firstName+' '+user.lastName,
+             link:text
+        };
+        let htmlToSend = template(replacements);
+   
     // send email
 
-    const mailOptions =
+   return  mailOptions =
     {
         to: user.email,
         from: FROM_EMAIL,
         subject: subject,
-        text: text,
+        attachments: [{
+            filename: 'logo.svg',
+            path: path.join(__dirname, '../public/img/logo.svg'),
+            cid: 'unique@kreata.ee' //same cid value as in the html img src
+        }],
+    
+        //text: text,
+        html : htmlToSend,
+   
     };
-    /*let transport = nodemailer.createTransport({
-           service: 'gmail',
-           auth: {
-               user: FROM_EMAIL,
-               pass: PSWD_EMAIL
-           } 
-       });*/
+});
+
 
     return await new Promise((resolve, reject) => {
-        let transport = nodemailer.createTransport({
+/*         let transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: FROM_EMAIL,
+                pass: PSWD_EMAIL
+            } 
+        }); */
+         let transport = nodemailer.createTransport({
             host: "smtp.mailtrap.io",
             port: 2525,
             auth: {
                 user: "b0852e2e5f728c",
                 pass: "b95b5c331a97f4"
             }
-        });
+        }); 
 
         transport.verify((error, success) => {
 

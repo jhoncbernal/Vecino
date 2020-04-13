9cdeee9167c4f2d65d000c4d894823eaffecd843
jhoncbernal <jhoncbernal@gmail.com>
2020-04-12 19:42:13 -0500

    [FIX]: add authmiddleware

--- src/routes/file.routes.js
+++ src/routes/file.routes.js
@@ -4,6 +4,6 @@ const {AuthMiddlewareAdmin}=require('../middlewares');
 module.exports=function({FileController}){
     const router=Router(); 
     router.post('',AuthMiddlewareAdmin, FileController.uploadFileCSV);
-    router.post('/images/upload', FileController.uploadFileImage);
+    router.post('/images/upload',AuthMiddlewareAdmin, FileController.uploadFileImage);
     return router;
 }

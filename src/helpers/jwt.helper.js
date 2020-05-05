042ec73ed1942de8b911eab31b649258e1d86d58
jhoncbernal <jhoncbernal@gmail.com>
2020-05-05 00:31:43 -0500

    [update]:expiresTime to 30 days

--- src/helpers/jwt.helper.js
+++ src/helpers/jwt.helper.js
@@ -2,13 +2,13 @@ const {sign}= require('jsonwebtoken');
 const {JWT_SECRET,JWT_SECRETOWNER,JWT_SECRETADMIN}= require('../config');
 
 function generateToken(user){
-    return sign({user},JWT_SECRET,{expiresIn:"4h"});
+    return sign({user},JWT_SECRET,{expiresIn:"30d"});
 }
 function generateTokenAdmin(user){
-    return sign({user},JWT_SECRETADMIN,{expiresIn:"8h"});
+    return sign({user},JWT_SECRETADMIN,{expiresIn:"30d"});
 }
 function generateTokenOwner(user){
-    return sign({user},JWT_SECRETOWNER,{expiresIn:"4h"});
+    return sign({user},JWT_SECRETOWNER,{expiresIn:"30d"});
 }
 module.exports={
     generateToken,

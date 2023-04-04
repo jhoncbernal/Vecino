c9a9c23246804f0ea3fc19b3d52ca4d89faa28da
Jhon Casallas <jhoncbernal@gmail.com>
2023-04-04 00:48:35 -0400

    feat: add ses aws

--- src/helpers/send-email.helper.js
+++ src/helpers/send-email.helper.js
@@ -1,101 +1,95 @@
-const nodemailer = require('nodemailer');
-var handlebars = require('handlebars');
-var fs = require('fs');
-var path = require('path');
-const { FROM_EMAIL, PSWD_EMAIL,BLNMAILTRAP } = require('../config');
+const aws = require("aws-sdk");
+const handlebars = require("handlebars");
+const fs = require("fs");
+const path = require("path");
+const {
+  FROM_EMAIL,
+  AWSREGION,
+  AWSSECRETACCESSKEY,
+  AWSACCESSKEYID,
+} = require("../config");
+aws.config.update({
+  accessKeyId: AWSACCESSKEYID,
+  secretAccessKey: AWSSECRETACCESSKEY,
+  region: AWSREGION,
+});
+const ses = new aws.SES({ region: AWSREGION });
 
 async function readHTMLFile(path, callback) {
-    fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
+  fs.readFile(path, { encoding: "utf-8" }, function (err, html) {
+    if (err) {
+      throw err;
+    } else {
+      callback(null, html);
+    }
+  });
+}
+
+async function HTMLReplace(htmlpath, replacements) {
+  try {
+    return (htmlToSend = await new Promise((resolve, reject) => {
+      readHTMLFile(path.join(__dirname, htmlpath), function (err, html) {
+        var template = handlebars.compile(html);
+
         if (err) {
-            throw err;
+          reject(err);
+        } else {
+          resolve(template(replacements));
         }
-        else {
-            callback(null, html);
-        }
-    });
+      });
+    }));
+  } catch (err) {
+    throw err;
+  }
 }
-async function HTMLReplace(htmlpath,replacements){
-    try {
-        return htmlToSend = await new Promise((resolve, reject) => {
-     
-            readHTMLFile(path.join(__dirname, htmlpath), function (err, html) {
-                var template = handlebars.compile(html);
-                
-                if (err) {
-                    reject(err);
-                }
-                else {
-                    resolve(template(replacements));
-                }
-            });
-        });
-    }catch(err){
-        throw error
-    }
-}
+
 async function sendEmail(user, subject, text, htmlpath) {
-    try {
-        htmlToSend = await new Promise((resolve, reject) => {
-            readHTMLFile(path.join(__dirname, htmlpath), function (err, html) {
-                var template = handlebars.compile(html);
-                var replacements = {
-                    username: user.firstName,
-                    link: text
-                };
-                if (err) {
-                    reject(err);
-                }
-                else {
-                    resolve(template(replacements));
-                }
-            });
-        });
-        const mailOptions =
-        {
-            to: user.email,
-            from: FROM_EMAIL,
-            subject: subject,
-            html: htmlToSend,
+  try {
+    htmlToSend = await new Promise((resolve, reject) => {
+      readHTMLFile(path.join(__dirname, htmlpath), function (err, html) {
+        var template = handlebars.compile(html);
+        var replacements = {
+          username: user.firstName,
+          link: text,
         };
+        if (err) {
+          reject(err);
+        } else {
+          resolve(template(replacements));
+        }
+      });
+    });
+    const mailOptions = {
+      Destination: {
+        ToAddresses: [user.email],
+      },
+      Message: {
+        Body: {
+          Html: {
+            Data: htmlToSend,
+            Charset: "utf-8",
+          },
+        },
+        Subject: {
+          Data: subject,
+          Charset: "utf-8",
+        },
+      },
+      Source: FROM_EMAIL,
+    };
 
-        return await new Promise((resolve, reject) => {
-            let transport;
-            if (BLNMAILTRAP!="no") {
-                transport = nodemailer.createTransport({
-                    host: "smtp.mailtrap.io",
-                    port: 2525,
-                    auth: {
-                        user: "b0852e2e5f728c",
-                        pass: "b95b5c331a97f4"
-                    }
-                });
-            } else {
-                transport = nodemailer.createTransport({
-                    service: 'gmail',
-                    auth: {
-                        user: FROM_EMAIL,
-                        pass: PSWD_EMAIL
-                    }
-                });
-            }
-            transport.verify((error, success) => {
-
-                if (error) {
-                    reject(error);
-                } else {
-                    transport.sendMail(mailOptions, function (err, info) {
-                        if (err) {
-                            reject(err);
-                        } else {
-                            resolve(info);
-                        }
-                    })
-                }
-            })
-        });
-    }
-    catch (error) {
-        throw error
-    }
+    return await new Promise((resolve, reject) => {
+      ses.sendEmail(mailOptions, function (err, info) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(info);
+        }
+      });
+    });
+  } catch (error) {
+    throw error;
+  }
 }
-module.exports = {sendEmail,HTMLReplace};
+
+module.exports = { sendEmail, HTMLReplace };

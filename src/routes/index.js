923655b527d51705519fc45019ed26b420cc24e5
Jhon Casallas <jhoncbernal@gmail.com>
2023-04-20 03:02:20 -0400

    add: cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,57 +1,58 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const compression = require('compression');
-require('express-async-errors');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+require("express-async-errors");
 const { ErrorMiddleware, NotFoundMiddleware } = require("../middlewares");
 //fileupload
-var fileUpload = require('express-fileupload');
+var fileUpload = require("express-fileupload");
 
 module.exports = function ({
-    UserRoutes,
-    AuthRoutes,
-    AdminRoutes,
-    ProviderRoutes,
-    VehicleRoutes,
-    ParkingSpaceRoutes,
-    PositionsRoutes,
-    FileRoutes,
-    ProductRoutes,
-    BillRoutes,
-    CityRoutes,
-    PackageRoutes
+  UserRoutes,
+  AuthRoutes,
+  AdminRoutes,
+  ProviderRoutes,
+  VehicleRoutes,
+  ParkingSpaceRoutes,
+  PositionsRoutes,
+  FileRoutes,
+  ProductRoutes,
+  BillRoutes,
+  CityRoutes,
+  PackageRoutes,
 }) {
-    const router = express.Router();
-    const apiRoutes = express.Router();
+  const router = express.Router();
+  const apiRoutes = express.Router();
+  // CORS configuration
+  const corsOptions = {
+    origin: ["corsOptions", "http://localhost:3000"], // Replace with your Heroku app URL and any other origins you want to allow
+    methods: "GET,PUT,PATCH,POST,DELETE", // Allowed methods
+    allowedHeaders: "Content-Type,Authorization", // Allowed headers
+  };
+  apiRoutes
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }))
+    .use(cors(corsOptions))
+    .use(helmet())
+    .use(compression())
+    .use(fileUpload());
 
-    apiRoutes
-        .use(express.json())
-        .use(express.urlencoded({ extended: true }))
-        .use('*', cors())
-        .use(helmet())
-        .use(compression())
-        .use(fileUpload());
+  apiRoutes.use("/user", UserRoutes);
+  apiRoutes.use("/auth", AuthRoutes);
+  apiRoutes.use("/admin", AdminRoutes);
+  apiRoutes.use("/provider", ProviderRoutes);
+  apiRoutes.use("/vehicle", VehicleRoutes);
+  apiRoutes.use("/parkingspace", ParkingSpaceRoutes);
+  apiRoutes.use("/parkingspace/positions", PositionsRoutes);
+  apiRoutes.use("/file", FileRoutes);
+  apiRoutes.use("/product", ProductRoutes);
+  apiRoutes.use("/bill", BillRoutes);
+  apiRoutes.use("/city", CityRoutes);
+  apiRoutes.use("/package", PackageRoutes);
 
+  router.use("/v1/api", apiRoutes);
+  router.use(NotFoundMiddleware);
+  router.use(ErrorMiddleware);
 
-    apiRoutes.use('/user', UserRoutes);
-    apiRoutes.use('/auth', AuthRoutes);
-    apiRoutes.use('/admin', AdminRoutes);
-    apiRoutes.use('/provider', ProviderRoutes);
-    apiRoutes.use('/vehicle', VehicleRoutes);
-    apiRoutes.use('/parkingspace', ParkingSpaceRoutes);
-    apiRoutes.use('/parkingspace/positions', PositionsRoutes);
-    apiRoutes.use('/file', FileRoutes);
-    apiRoutes.use('/product', ProductRoutes);
-    apiRoutes.use('/bill', BillRoutes);
-    apiRoutes.use('/city', CityRoutes);
-    apiRoutes.use('/package', PackageRoutes);
-
-
-
-    router.use('/v1/api', apiRoutes);
-    router.use(NotFoundMiddleware);
-    router.use(ErrorMiddleware);
-
-
-    return router;
-}
+  return router;
+};
